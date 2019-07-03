31beef8f1a4cc7ac0a3a40cae84da4c2c46514ab
Jason Wong <jwong175@hotmail.com>
2019-07-02 23:14:20 -0700

    added pet gif, name, and very basic styling

--- js/app.js
+++ js/app.js
@@ -2,9 +2,16 @@
 class Tamagotchi {
     constructor(name) {
         this.name = name;
+        this.age = 0;
         this.hungerLevel = 100;
         this.energyLevel = 100;
         this.playLevel = 100;
+    }
+    initPet(){
+        console.log('Initializing tamagotchi');
+        $('#pet').append('<img id="thePet" src="https://66.media.tumblr.com/d8d9ec566f240850f35c703b4b2a42c0/tumblr_or6nwehp7T1wq3wtao4_250.gif"/>');
+        const $h1 = $(`<h1>${this.name}</h1>`)
+        $('#pet').append($h1);
     }
     feed() {
         this.hungerLevel += 50;
@@ -36,6 +43,7 @@ class Tamagotchi {
 $('button').on('click', () => {
     console.log('Button works!')
     game.createPet();
+    $('button').hide();
 })
 
 
@@ -46,5 +54,6 @@ const game = {
         alert(`${petName} has been born!`)
         const newPet = new Tamagotchi(petName);
         console.log(newPet)
+        newPet.initPet();
     }
 }
